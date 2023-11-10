9395578084751a8b82ccd3b6c9cfff0f066f2b42
Zakhele026 <Zakhelegwamanda026@outlook.com>
2023-11-10 13:43:40 +0200

    Simple Shell

---
+++ errors1.c
@@ -0,0 +1,144 @@
+#include "shell.h"
+
+/**
+ * _erratoi - changes the string to an integer
+ * @s: the changed string 
+ * Return: 0 if no numbers in the string, changed number or
+ *       -1 for an error
+ */
+int _erratoi(char *s)
+{
+	int i = 0;
+	unsigned long int result = 0;
+
+	if (*s == '+')
+		s++;  /* TODO: why does this make main return 255? */
+	for (i = 0;  s[i] != '\0'; i++)
+	{
+		if (s[i] >= '0' && s[i] <= '9')
+		{
+			result *= 10;
+			result += (s[i] - '0');
+			if (result > INT_MAX)
+				return (-1);
+		}
+		else
+			return (-1);
+	}
+	return (result);
+}
+
+/**
+ * print_error - Prints an error message to the standard error stream.
+ * @info: Pointer to the structure containing 
+ * 		parameter and return information.
+ * @estr: string that contains specified type of error
+ * Return:  Return: 0 on successful printing, -1 for an error.
+ */
+
+void print_error(info_t *info, char *estr)
+{
+	_eputs(info->fname);
+	_eputs(": ");
+	print_d(info->line_count, STDERR_FILENO);
+	_eputs(": ");
+	_eputs(info->argv[0]);
+	_eputs(": ");
+	_eputs(estr);
+}
+
+/**
+ * print_d - will print a decimal number (base 10)
+ * @input: The decimal number to be printed
+ * @fd: The file descriptor where the output is written
+ *
+ * Return: characters printed
+ */
+
+int print_d(int input, int fd)
+{
+	int (*__putchar)(char) = _putchar;
+	int i, count = 0;
+	unsigned int _abs_, current;
+
+	if (fd == STDERR_FILENO)
+		__putchar = _eputchar;
+	if (input < 0)
+	{
+		_abs_ = -input;
+		__putchar('-');
+		count++;
+	}
+	else
+		_abs_ = input;
+	current = _abs_;
+	for (i = 1000000000; i > 1; i /= 10)
+	{
+		if (_abs_ / i)
+		{
+			__putchar('0' + current / i);
+			count++;
+		}
+		current %= i;
+	}
+	__putchar('0' + current);
+	count++;
+
+	return (count);
+}
+
+/**
+ * convert_number - converter function (clone of itoa)
+ * @num: number
+ * @base: base
+ * @flags: arg flags
+ *
+ * Return: string
+ */
+
+char *convert_number(long int num, int base, int flags)
+{
+	static char *array;
+	static char buffer[50];
+	char sign = 0;
+	char *ptr;
+	unsigned long n = num;
+
+	if (!(flags & CONVERT_UNSIGNED) && num < 0)
+	{
+		n = -num;
+		sign = '-';
+
+	}
+	array = flags & CONVERT_LOWERCASE ? "0123456789abcdef" : "0123456789ABCDEF";
+	ptr = &buffer[49];
+	*ptr = '\0';
+
+	do	{
+		*--ptr = array[n % base];
+		n /= base;
+	} while (n != 0);
+
+	if (sign)
+		*--ptr = sign;
+	return (ptr);
+}
+
+/**
+ * remove_comments - function will replace '#' with '\0' at first occurance
+ * @buf: location of the string to be modified
+ *
+ * Return: Everytime 0;
+ */
+
+void remove_comments(char *buf)
+{
+	int i;
+
+	for (i = 0; buf[i] != '\0'; i++)
+		if (buf[i] == '#' && (!i || buf[i - 1] == ' '))
+		{
+			buf[i] = '\0';
+			break;
+		}
+}
